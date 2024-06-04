d184f4793ba0df285049e7d71d73a4e34f5cd239
Nylecarg <gracelyn_gosal@s2023.ssts.edu.sg>
2024-06-04 19:23:19 +0800

    new proj!!

---
+++ scoutapp/scoutapp/scoutappApp.swift
@@ -0,0 +1,17 @@
+//
+//  scoutappApp.swift
+//  scoutapp
+//
+//  Created by Gracelyn Gosal on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct scoutappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
